8f9a39d756d87fe97cda379cbc2ecb50ecc7b789
KarmBinning <44921669+KarmBinning@users.noreply.github.com>
2023-01-15 05:12:24 -0500

    Initial commit

---
+++ CC0641_FlipCards.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * https://www.codechef.com/problems/FLIPCARDS
+ * C:\Users\Karm\Documents\Dev\CodeChef\641-FLIPCARDS\Problem.pdf
+ * file:///C:/Users/Karm/Documents/Dev/CodeChef/641-FLIPCARDS/Problem.pdf
+ * 
+*/
+
+using System;
+
+public class CC0641_FlipCards
+{
+    public static void Run()
+    {
+        //Console.WriteLine("Testing... MATH641");
+        int t = int.Parse(Console.ReadLine());
+        while (t-- > 0)
+        {
+            int[] z = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Console.WriteLine(Math.Min(z[0] - z[1], z[1]));
+        }
+    }
+}
